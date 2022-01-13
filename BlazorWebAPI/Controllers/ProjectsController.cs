608e8eedb2a2239ec24b5c207bfbc88707ffd898
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-01-13 15:34:50 +0100

    Add Controllers

--- BlazorWebAPI/Controllers/ProjectsController.cs
+++ BlazorWebAPI/Controllers/ProjectsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Models;
+using DataStore.EF;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BlazorWebAPI.Controllers
 {
@@ -6,37 +10,82 @@ namespace BlazorWebAPI.Controllers
     [Route("api/[controller]")]
     public class ProjectsController : ControllerBase
     {
+        private readonly BugsContext db;
+
+        public ProjectsController(BugsContext _db)
+        {
+            db = _db;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Reading all the projects");
+            return Ok(db.Projects.ToList());
         }
 
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            return Ok($"Reading the project #{id}.");
+            var project = db.Projects.Find(id);
+            if (project == null)
+                return NotFound();
+
+            return Ok(project);
+        }
+
+        [HttpGet]
+        [Route("/api/projects/{pid}/tickets")]
+        public IActionResult GetProjectsTickets(int pid)
+        {
+            var tickets = db.Tickets.Where(t => t.ProjectId == pid).ToList();
+            if (tickets == null || tickets.Count <= 0)
+                return NotFound();
+
+            return Ok(tickets);
         }
 
 
         [HttpPost]
-        public IActionResult Create()
+        public IActionResult Create([FromBody] Project project)
         {
-            return Ok("Creating a project");
+            db.Projects.Add(project);
+            db.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = project.ProjectId }, project );
         }
 
-        [HttpPut]
-        public IActionResult Update()
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Project project)
         {
-            return Ok("Updating a project");
+            if (id != project.ProjectId) return BadRequest();
+
+            db.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch
+            {
+                if (db.Projects.Find(id) == null)
+                    return NotFound();
+            }
+
+            return NoContent();
         }
 
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            return Ok($"Deleting a project #{id}.");
+            var project = db.Projects.Find(id);
+            if (project == null) return NotFound();
+
+            db.Projects.Remove(project);
+            db.SaveChanges();
+
+            return Ok(project);
         }
     }
 }
--- BlazorWebAPI/Controllers/TicketsController.cs
+++ BlazorWebAPI/Controllers/TicketsController.cs
@@ -1,5 +1,8 @@
 ﻿using Core.Models;
+using DataStore.EF;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BlazorWebAPI.Controllers
 {
@@ -7,38 +10,74 @@ namespace BlazorWebAPI.Controllers
     [Route("api/[controller]")]
     public class TicketsController : ControllerBase
     {
+        private readonly BugsContext db;
+
+        public TicketsController(BugsContext _db)
+        {
+            db = _db;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Reading all the tickets");
+            return Ok(db.Tickets.ToList());
         }
 
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            return Ok($"Reading the ticket #{id}.");
+            var ticket = db.Tickets.Find(id);
+            if (ticket == null)
+                return BadRequest();
+
+            return Ok(ticket);
         }
 
 
         [HttpPost]
         public IActionResult Create([FromBody] Ticket ticket)
         {
-            return Ok(ticket);
+            db.Tickets.Add(ticket);
+            db.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = ticket.TicketId }, ticket);
         }
 
 
-        [HttpPut]
-        public IActionResult Update([FromBody] Ticket ticket)
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Ticket ticket)
         {
-            return Ok(ticket);
+            if (id != ticket.TicketId) return BadRequest();
+
+            db.Entry(ticket).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch
+            {
+                if (db.Tickets.Find(id) == null)
+                    return NotFound();
+            }
+
+            return NoContent();
         }
 
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            return Ok($"Deleting a ticket #{id}.");
+            var ticket = db.Tickets.Find(id);
+            if (ticket == null)
+                return NotFound();
+
+            db.Tickets.Remove(ticket);
+            db.SaveChanges();
+
+
+            return Ok(ticket);
         }
     }
 }
